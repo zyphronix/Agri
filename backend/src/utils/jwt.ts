e1db3a73300c0817eed3c4abb51a25e8982c33d2
Lakshya Dhaka <lakshyadhaka.987s@gmail.com>
2025-11-20 05:26:07 +0530

    fixing deploy issue

--- backend/src/utils/jwt.ts
+++ backend/src/utils/jwt.ts
@@ -1,11 +1,11 @@
-import jwt, { SignOptions, Secret } from 'jsonwebtoken';
+import jwt, { SignOptions, Secret, StringValue } from 'jsonwebtoken';
 import config from '../config/env';
 
 export const generateToken = (userId: string, phone: string): string => {
   const payload = { id: userId, phone };
 
   const options: SignOptions = {
-    expiresIn: config.jwtExpiresIn, // must be string | number
+    expiresIn: config.jwtExpiresIn as unknown as StringValue,
   };
 
   return jwt.sign(payload, config.jwtSecret as Secret, options);
